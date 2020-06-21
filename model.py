2c72f52c8cc2fbd422874187692d8e488b74c08b
Chrobot <kuba.chrobocinski@gmail.com>
2020-06-21 23:44:16 +0200

    working unet 50 cases

--- model.py
+++ model.py
@@ -2,20 +2,21 @@
 from starter_code.utils import load_segmentation
 import numpy as np
 import nibabel
-import matplotlib.pyplot as plt
-import numexpr as ne
-from joblib import Parallel, delayed
-import multiprocessing
-import os
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.linear_model import SGDClassifier
 import pandas as pd
 import tensorflow as tf
-from tensorflow_examples.models.pix2pix import pix2pix
+import numpy as np 
+import os
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
 from skimage import color
-import cv2
-from IPython.display import clear_output
-#from tensorflow.examples.models.pix2pix import pix2pix
+import matplotlib.pyplot as plt
+#tf.compat.v1.disable_eager_execution()
 
 
 #%%
@@ -24,9 +25,9 @@ def get_data(case_nr):
 
     def get_case(case_nr):
         
-        os.chdir("c:\\Users\\Chrobot\\Desktop\\TOM\\Projekt\\kits19\\preprocessed_added_dummy")
+        os.chdir("c:\\Users\\Chrobot\\Desktop\\TOM\\Projekt\\kits19\\preprocessed")
 
-        case_nb = nibabel.load('case{}_preprocessed_added.nii.gz'.format(case_nr))
+        case_nb = nibabel.load('case{}_preprocessed.nii.gz'.format(case_nr))
         case = case_nb.get_fdata()
 
         return case
@@ -35,7 +36,7 @@ def get_data(case_nr):
         
         os.chdir("c:\\Users\\Chrobot\\Desktop\\TOM\\Projekt\\kits19\\segment_added_dummy")
 
-        segment_nb = nibabel.load('segmentation{}_added.nii.gz'.format(case_nr))
+        segment_nb = load_segmentation(case_nr)
         segment = segment_nb.get_fdata()
 
         return segment
@@ -45,64 +46,6 @@ def get_data(case_nr):
     return X,Y
 
 #%%
-base_model = tf.keras.applications.MobileNetV2(input_shape=[512, 512, 3], include_top=False)
-
-# Use the activations of these layers
-layer_names = [
-    'block_1_expand_relu',   # 64x64
-    'block_3_expand_relu',   # 32x32
-    'block_6_expand_relu',   # 16x16
-    'block_13_expand_relu',  # 8x8
-    'block_16_project',      # 4x4
-]
-layers = [base_model.get_layer(name).output for name in layer_names]
-
-# Create the feature extraction model
-down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
-
-down_stack.trainable = False
-OUTPUT_CHANNELS = 3
-#%%
-up_stack = [
-    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
-    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
-    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
-    pix2pix.upsample(64, 3),   # 32x32 -> 64x64
-]
-
-def unet_model(output_channels):
-  inputs = tf.keras.layers.Input(shape=[512, 512, 3])
-  x = inputs
-
-  # Downsampling through the model
-  skips = down_stack(x)
-  x = skips[-1]
-  skips = reversed(skips[:-1])
-
-  # Upsampling and establishing the skip connections
-  for up, skip in zip(up_stack, skips):
-    x = up(x)
-    concat = tf.keras.layers.Concatenate()
-    x = concat([x, skip])
-
-  # This is the last layer of the model
-  last = tf.keras.layers.Conv2DTranspose(
-      output_channels, 3, strides=2,
-      padding='same')  #64x64 -> 128x128
-
-  x = last(x)
-
-  return tf.keras.Model(inputs=inputs, outputs=x)
-
-
-# %%
-model = unet_model(OUTPUT_CHANNELS)
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-tf.keras.utils.plot_model(model, show_shapes=True)
-#%%
 def display(display_list):
   plt.figure(figsize=(15, 15))
 
@@ -111,17 +54,9 @@ def display(display_list):
   for i in range(len(display_list)):
     plt.subplot(1, len(display_list), i+1)
     plt.title(title[i])
-    plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+    plt.imshow(display_list[i][:,:,0], cmap="gray")
     plt.axis('off')
   plt.show()
-
-#%%
-image, mask = get_data(4)
-image = image[20,:,:]
-mask = mask[20,:,:]
-
-sample_image = color.gray2rgb(image)
-sample_mask = color.gray2rgb(mask)
 
 #%%
 
@@ -132,46 +67,66 @@ def create_mask(pred_mask):
 
 def show_predictions(dataset=None, num=1):
   if dataset:
-    for image, mask in dataset.take(num):
+    for image, mask in dataset:
       pred_mask = model.predict(image)
       display([image[0], mask[0], create_mask(pred_mask)])
   else:
     display([sample_image, sample_mask,
              create_mask(model.predict(sample_image[tf.newaxis, ...]))])
 
-show_predictions()
-
 #%%
-train_data, train_mask  = get_data(4)
-test_data, test_mask = get_data(5)
-
-train_data = np.stack((train_data,train_data,train_data), axis = 3).astype(np.float16)
-train_mask = np.stack((train_mask,train_mask,train_mask), axis = 3).astype(np.float16)
-test_data = np.stack((test_data,test_data,test_data), axis = 3).astype(np.float16)
-test_mask = np.stack((test_mask,test_mask,test_mask), axis = 3).astype(np.float16)
-
-train_dataset = (train_data,train_mask) 
-test_dataset = (test_data, test_mask)
-#%%
-class DisplayCallback(tf.keras.callbacks.Callback):
-  def on_epoch_end(self, epoch, logs=None):
-    clear_output(wait=True)
-    show_predictions()
-    print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
 
 
-BATCH_SIZE = 139
-BUFFER_SIZE = 1000
-STEPS_PER_EPOCH = 834// BATCH_SIZE
+def unet(pretrained_weights = None,input_size = (512,512,1)):
+    inputs = Input(input_size)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
-EPOCHS = 20
-VAL_SUBSPLITS = 5
-VALIDATION_STEPS = 834//BATCH_SIZE//VAL_SUBSPLITS
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    drop5 = Dropout(0.5)(conv5)
 
-model_history = model.fit(train_dataset, epochs=EPOCHS,
-                          steps_per_epoch=STEPS_PER_EPOCH,
-                          validation_steps=VALIDATION_STEPS,
-                          validation_data=test_dataset,
-                          callbacks=[DisplayCallback()])
+    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+    merge6 = concatenate([drop4,up6], axis = 3)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
 
-# %%
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+    merge7 = concatenate([conv3,up7], axis = 3)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+    merge8 = concatenate([conv2,up8], axis = 3)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+    merge9 = concatenate([conv1,up9], axis = 3)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+
+    model = Model(input = inputs, output = conv10)
+
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    
+    #model.summary()
+
+    if(pretrained_weights):
+    	model.load_weights(pretrained_weights)
+
+    return model
+
---
+++ preprocessing.py
@@ -0,0 +1,97 @@
+def preprocessing(case_nr, slice_number_to_print):
+
+    from starter_code.utils import load_volume
+    import numpy as np
+    import nibabel
+    import matplotlib.pyplot as plt
+    from skimage import filters
+    from skimage.morphology import disk, square
+    from skimage import exposure
+    from skimage.morphology import disk, closing, opening, remove_small_holes
+    from skimage.color import label2rgb, rgb2gray
+    import numexpr as ne
+    from joblib import Parallel, delayed
+    import multiprocessing
+    import os
+
+    def normalize(image):
+        minimum = np.min(image)
+        maximum = np.max(image)
+        result = (image - minimum)/(maximum - minimum)
+        return result
+
+    volume = load_volume(case_nr)
+    #data_seg = segment.get_fdata()
+    data = volume.get_fdata()
+    data_preprocessed_vol = np.zeros(data.shape)
+    data_masks = np.zeros(data.shape)
+
+    slice_nr_list = list(range(data.shape[0])) #List to iterate by
+
+    #Definition of preprocessing per one slice
+    def slice_pre(data_slice): 
+
+        data_normalized = normalize(data_slice)
+        data_oryg = data_normalized.copy()
+        data_hist = exposure.equalize_adapthist(data_normalized, clip_limit = 0.1)
+
+        data_median = filters.median(data_hist, selem = disk(3))
+        data_median = exposure.adjust_gamma(data_median, gamma = 10)
+
+        thresholds = filters.threshold_multiotsu(data_median)
+        regions = np.digitize(data_normalized, bins=thresholds)
+
+        data_normalized[regions != 2] = 0
+
+        data_pre = exposure.equalize_hist(data_normalized)
+
+        thresholds = filters.threshold_multiotsu(data_pre)
+
+        regions = np.digitize(data_pre, bins=thresholds)
+        data_mask = label2rgb(regions)
+        data_mask = rgb2gray(data_mask)
+        data_mask[data_mask != np.max(data_mask)] = 0
+        data_mask[data_mask == np.max(data_mask)] = 1
+        data_mask = closing(data_mask, selem = disk(3)).astype(int)
+        data_mask = remove_small_holes(data_mask, area_threshold=300)
+
+        data_eq = exposure.equalize_adapthist(data_oryg, clip_limit = 0.15)
+        data_preprocessed = data_eq*data_mask
+        data_preprocessed = opening(data_preprocessed, selem = disk(3))
+
+        return data_preprocessed, data_mask
+
+    num_cores = multiprocessing.cpu_count()
+    data_list = Parallel(n_jobs=num_cores)(delayed(slice_pre)(data[slice_nr,:,:]) for slice_nr in slice_nr_list)
+    data_arr = np.array(data_list)
+    data_preprocessed_vol = data_arr[:,0,:,:]
+    data_masks = data_arr[:,1,:,:]
+
+    path = os.getcwd()
+    new_path = os.path.join(path, "preprocessed")
+    os.chdir(new_path)
+
+    img_pre = nibabel.Nifti1Image(data_preprocessed_vol, volume.affine)
+    nibabel.save(img_pre,'case{}_preprocessed.nii.gz'.format(case_nr))
+    #Saving to file
+    os.chdir(path)
+
+    #This part prints slice if user specifies slice number
+    if slice_number_to_print != -1:
+        fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(10, 3.5))
+        fig.suptitle('Case: {}, Slice: {}'.format(case_nr, slice_number_to_print), fontsize=16)
+        ax[0].imshow(data[slice_number_to_print,:,:]  , cmap='gray')
+        ax[0].set_title('Original')
+        ax[0].axis('off')
+
+        ax[1].imshow(data_masks[slice_number_to_print,:,:], cmap='gray')
+        ax[1].set_title('mask')
+        ax[1].axis('off')
+
+        ax[2].imshow(data_preprocessed_vol[slice_number_to_print,:,:], cmap='gray')
+        ax[2].set_title('preprocessed')
+        ax[2].axis('off')
+
+        plt.subplots_adjust()
+
+        plt.show()
